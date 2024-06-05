069ff3cc6d4dbfb61cd50a49e6606ed1f4ed5847
unknown <edgaromerobula@gmail.com>
2024-06-04 20:49:27 -0500

    subimos proyecto inicial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/democrud-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/project/democrud/controller/ProductoController.java
@@ -0,0 +1,57 @@
+package com.project.democrud.controller;
+
+import com.project.democrud.model.Producto;
+import com.project.democrud.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> getAllProductos() {
+        return productoService.getAllProductos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> getProductoById(@PathVariable Long id) {
+        Optional<Producto> producto = productoService.getProductoById(id);
+        return producto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Producto createProducto(@RequestBody Producto producto) {
+        return productoService.saveProducto(producto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> updateProducto(@PathVariable Long id, @RequestBody Producto productoDetails) {
+        Optional<Producto> producto = productoService.getProductoById(id);
+        if (producto.isPresent()) {
+            Producto updatedProducto = producto.get();
+            updatedProducto.setNombre(productoDetails.getNombre());
+            updatedProducto.setPrecio(productoDetails.getPrecio());
+            productoService.saveProducto(updatedProducto);
+            return ResponseEntity.ok(updatedProducto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProducto(@PathVariable Long id) {
+        if (productoService.getProductoById(id).isPresent()) {
+            productoService.deleteProducto(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
